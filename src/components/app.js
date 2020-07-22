ae196aff6e76e52fb555e57d1cdf379c99050690
Heith <Heith.Robbins@gmail.com>
2020-07-22 11:07:09 -0600

    created malib form component

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 
+import MadlibForm from './madlib_form';
+
 export default class App extends Component {
   render() {
     return (
       <div>
         <div className="backgroundSkew">
           <div className="madlib-heading">
-          <h1>Mad Libs</h1>
+            <h1>Mad Libs</h1>
           <div className="madlib-subheading">
             Fill out the field below and click the genrate button<br/>to see the mad lib story.
           </div>
-
           </div>
+            <MadlibForm />
         </div>
         
       </div>
